78da9af8e9643cef875e8b0d0bd8e5e44bb3434b
just-noname-bing <danikfedorov41@gmail.com>
2023-05-25 02:10:14 +0300

    connected backend and fronted

--- frontend/components/Posts.tsx
+++ frontend/components/Posts.tsx
@@ -1,13 +1,35 @@
 import React from "react"
 import { Center } from "./styled/Center"
-import { Text } from 'react-native'
+import { ActivityIndicator, Text, View } from 'react-native'
+import { useQuery } from "@apollo/client/react"
+import { gql } from "@apollo/client/core"
 
 interface Props { }
 
+const POSTS_QUERY = gql`
+    query GetPosts {
+      getPosts {
+        id
+        text
+        title
+        transport_id
+        created_at
+        updated_at
+      }
+}`
+
 export function Posts(_: Props): JSX.Element {
+    const { data: posts, loading, error } = useQuery(POSTS_QUERY)
     return (
         <Center>
-            <Text>bomb</Text>
+            {!loading ? (
+                <View>
+                    <Text>{JSON.stringify(posts, null, 2)}</Text>
+                    <Text>{JSON.stringify(error)}</Text>
+                </View>
+            ) : (
+                <ActivityIndicator size="large" color="#0000ff" />
+            )}
         </Center>
     )
 }
---
+++ frontend/utils/constants.ts
@@ -0,0 +1,1 @@
+export const GRAPHQL_API_URL = "http://192.168.118.132:4000/"
